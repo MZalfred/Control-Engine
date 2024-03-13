427110b30288a080d23f850ebf457e236b0b1521
MZalfred <cutesy-todo0x@icloud.com>
2024-03-13 15:59:58 +0000

    Initial Commit

---
+++ Control Engine/Control_EngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Control_EngineApp.swift
+//  Control Engine
+//
+//  Created by Alfred Kafurama on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Control_EngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Control EngineUITests/Control_EngineUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Control_EngineUITestsLaunchTests.swift
+//  Control EngineUITests
+//
+//  Created by Alfred Kafurama on 13/03/2024.
+//
+
+import XCTest
+
+final class Control_EngineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
